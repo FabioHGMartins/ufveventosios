44aaf0d99ce63eef9ed465ac0133deccee21d46a
Fabio Martins <fabiomartins@Mac-mini-de-DPI.local>
2018-08-31 14:14:48 -0300

    Initial Commit

---
+++ SiseventosUFViOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SiseventosUFViOS
+//
+//  Created by Fabio Martins on 31/08/18.
+//  Copyright © 2018 Fabio Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
